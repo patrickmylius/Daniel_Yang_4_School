81f8385f6902aa900f81ac01dbc558d5d1605163
patrickmylius <patrickmylius@outlook.dk>
2020-09-05 15:41:25 +0200

    Added exercise 01_03

---
+++ src/Chap1/Exercise_01_03.java
@@ -0,0 +1,11 @@
+package Chap1;
+
+public class Exercise_01_03 {
+    public static void main(String[] args) {
+        //1.3
+        System.out.println("    J");
+        System.out.println("J  aaa   v   vaaa");
+        System.out.println("J  J  aa  v v   a a");
+        System.out.println("J   aaaa   v    aaaa");
+    }
+}
