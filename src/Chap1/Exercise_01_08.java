c4c82c4364e715349869cec61d577c7bd633e6ff
Patrick <patr6996@edu.easj.dk>
2020-09-07 11:42:34 +0200

    added exercise 01_08 + 01_09

---
+++ src/Chap1/Exercise_01_08.java
@@ -0,0 +1,13 @@
+package Chap1;
+//Area and perimeter of a circle
+public class Exercise_01_08 {
+    public static double pi = 3.14159;
+    public static double radius = 6.5;
+    public static void main(String[] args) {
+        System.out.println("perimeter = 2 * radius * pi");
+        System.out.println("perimeter = " + 2 * radius * pi);
+
+        System.out.println("area = radius * radius * pi");
+        System.out.println("area = " + radius * radius * pi);
+    }
+}
---
+++ src/Chap1/Exercise_01_09.java
@@ -0,0 +1,16 @@
+package Chap1;
+
+//Area and perimeter of a rectangle
+public class Exercise_01_09 {
+
+    static double width = 5.3;
+    static double height = 8.6;
+
+    public static void main(String[] args) {
+        System.out.println("area = width * height");
+        System.out.println("area = " + width * height);
+
+        System.out.println("Perimeter = 2 * (width + height)");
+        System.out.println("Perimeter =" + 2 * (width + height));
+    }
+}
