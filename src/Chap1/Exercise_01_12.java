7ef7699d6d97f352280304822b72139f97e15f82
Patrick <patr6996@edu.easj.dk>
2020-09-09 10:56:47 +0200

    Fixed result of exercise 01_12

--- src/Chap1/Exercise_01_12.java
+++ src/Chap1/Exercise_01_12.java
@@ -6,6 +6,6 @@ public class Exercise_01_12 {
         //1.12
         System.out.println("Miles / (hour + (minutes / 60) + (seconds / 3600)) * 1.6");
         System.out.println("24 / (1 + (40 / 60) + (35 / 3600)) * 1.6");
-        System.out.println(24 / (1 + (40 / 60) + (35 / 3600)) * 1.6);
+        System.out.println((24 / (1 + (40 / 60.0) + (35 / 3600.0))) * 1.6);
     }
 }
