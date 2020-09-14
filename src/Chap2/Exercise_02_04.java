a405c1d80076fa7e27d3835ef033f2ded5590290
Patrick <patr6996@edu.easj.dk>
2020-09-14 10:55:45 +0200

    Løsning til opgave 02_04

--- src/Chap2/Exercise_02_04.java
+++ src/Chap2/Exercise_02_04.java
@@ -1,4 +1,23 @@
 package Chap2;
+// (Convert square meter into ping)
+// Write a program that converts square meter into ping.
+// The program prompts the user to enter a number in square meter,
+// converts it to ping, and displays the result.
+// 1 square meter is 0.3025 ping
+import java.util.Scanner;
 
 public class Exercise_02_04 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //souts for an user input
+        System.out.println("Enter a number in square meters: ");
+        //sets ping to be 0.3025;
+        double ping = 0.3025;
+        //sets squareMeter to user input
+        double squareMeter = input.nextDouble();
+        //sets squareToPing to user input * 0,3025;
+        double squareToPing = squareMeter * ping;
+        //souts the converted answer.
+        System.out.println(squareMeter + "square meters converted is " + squareToPing + " " + "ping");
+    }
 }
