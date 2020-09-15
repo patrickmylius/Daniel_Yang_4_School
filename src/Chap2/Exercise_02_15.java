f2b7d4d95a8211d2f906a3527fc6e078227fdaea
patrickmylius <patrickmylius@outlook.dk>
2020-09-15 19:19:48 +0200

    Exercise_02_15 løsning m. noter

---
+++ src/Chap2/Exercise_02_15.java
@@ -0,0 +1,22 @@
+package Chap2;
+// Write a program that prompts the user to enter two points (x1, y1)
+// and (x2, y2) and displays their distance.
+import java.util.Scanner;
+
+public class Exercise_02_15 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //souts for x1, x2 and y1, y2 and sets them to variable double
+        System.out.print("Enter x1 and y1: ");
+        double x1 = input.nextDouble();
+        double y1 = input.nextDouble();
+        System.out.print("Enter x2 and y2");
+        double x2 = input.nextDouble();
+        double y2 = input.nextDouble();
+        //compute the distance between the two points.
+        double distance = Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), 0.5);
+        //souts the answer
+        System.out.println("The distance between the two points is: " + distance);
+    }
+}
