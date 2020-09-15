2f47795548228becb20c4e4eb784bf565015f050
Patrick <patr6996@edu.easj.dk>
2020-09-15 09:23:47 +0200

    Exercise_02_09 løsning m. noter

--- src/Chap2/Exercise_02_09.java
+++ src/Chap2/Exercise_02_09.java
@@ -1,4 +1,21 @@
 package Chap2;
 
+import java.util.Scanner;
+
 public class Exercise_02_09 {
+    public static void main(String[] args) {
+        //sets Scanner to input
+        Scanner input = new Scanner(System.in);
+        //souts user for an input
+        System.out.println("Enter v0, v1, and t: ");
+        //User input set to variable double v0, v1 and t.
+        double v0 = input.nextDouble();
+        double v1 = input.nextDouble();
+        double t = input.nextDouble();
+        //computes v1 - v0 / t and sets it to acceleration
+        double acceleration = (v1 - v0) / t;
+        //souts acceleration
+        System.out.println(acceleration);
+    }
+
 }
