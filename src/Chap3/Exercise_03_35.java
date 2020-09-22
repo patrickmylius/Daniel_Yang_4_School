db15b52dfaf1d8fb2076531ce9d03b25459a7a22
Patrick <patr6996@edu.easj.dk>
2020-09-22 13:36:02 +0200

    Exercise_03_35 solution with notes.
    Used differrent methods, first called from main.

--- src/Chap3/Exercise_03_35.java
+++ src/Chap3/Exercise_03_35.java
@@ -1,4 +1,41 @@
 package Chap3;
 
+import java.util.Scanner;
+
+/**
+ * (Even or odd number)
+ */
+// Write a program that prompts the user to enter an integer
+// and displays whether the integer is an odd number or not.
+
 public class Exercise_03_35 {
+    //Start
+    public static void main(String[] args) {
+        //Runs userInterface method
+        userInterface();
+
+    }
+
+    //Interface method obtaining variables and runs evenOrOdd method
+    static void userInterface() {
+        //prompt user to enter an integer
+        System.out.println("Enter an integer: ");
+        Scanner input = new Scanner(System.in);
+        //sets input to variable int "number"
+        int number = input.nextInt();
+
+        //Calls computed number in evenOrOdd method and sets to String "result"
+        String result = evenOrOdd(number);
+        //Prints computed number result
+        System.out.println(result);
+    }
+
+    //evenOrOdd method, computing with selektions if the number input is even or odd. returns number + string
+    static String evenOrOdd(int number) {
+        if (number % 2 == 0) {
+            return (number + ": is an even number");
+        } else {
+            return (number + ": is an odd number");
+        }
+    }
 }
