97749373240f26fa5fc2bfdd57f04afead05df70
patrickmylius <patrickmylius@outlook.dk>
2020-10-09 09:25:30 +0200

    Created folders with Main, UserInterface,Function classes for Chapter 6.

---
+++ src/Chap6/Exercise_06_27/Main.java
@@ -0,0 +1,9 @@
+/** Developed by Patrick Grønvold
+*   patr6996@edu.easj.dk
+*   Student of Computer Science
+*   Zealand ErhvervsAkademi - 4700 
+*   09-10-2020                       */
+package Chap6.Exercise_06_27;
+
+public class Main {
+}
