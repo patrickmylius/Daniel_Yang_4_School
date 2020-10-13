5f9be144e3b606dbde677d6709ece72dfdbe695b
patrickmylius <patrickmylius@outlook.dk>
2020-10-13 11:16:42 +0200

    Exercise_04_22 added w. notes

--- src/Chap4/Exercise_04_22.java
+++ src/Chap4/Exercise_04_22.java
@@ -7,6 +7,8 @@
  */
 package Chap4;
 
+import java.util.Scanner;
+
 /**
  * Check substring
  */
@@ -15,4 +17,21 @@ package Chap4;
 //of the first string
 
 public class Exercise_04_22 {
+    public static void main(String[] args) {
+        //Declares scanner to "input"
+        Scanner input = new Scanner(System.in);
+        //Prompts user for string 1 and string 2
+        //Declares them to s1 and s2
+        System.out.print("Enter a string s1: ");
+        String s1 = input.next();
+        System.out.print("Enter a string s2: ");
+        String s2 = input.next();
+
+        //If statement, checks wether s1 contains letters from s2
+        if (s1.contains(s2)) {
+            System.out.println(s2 + " " + "is a substring of " + s1);
+        }
+
+
+    }
 }
