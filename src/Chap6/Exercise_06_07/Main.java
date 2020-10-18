34d25a118fad872c118ff93606aa144be7775fbd
patrickmylius <patrickmylius@outlook.dk>
2020-10-18 15:55:56 +0200

    Chap6.Exercise_06_07 added w. Notes

--- src/Chap6/Exercise_06_07/Main.java
+++ src/Chap6/Exercise_06_07/Main.java
@@ -7,6 +7,8 @@
  */
 package Chap6.Exercise_06_07;
 
+import java.util.Scanner;
+
 /**
  * Financial application: compute the future investment value
  */
@@ -17,4 +19,33 @@ package Chap6.Exercise_06_07;
 //public static double futureInvestmenValue(double investmentAmount, double monthly InterestRate, int years)
 
 public class Main {
+    public static void main(String[] args) {
+        //sets variables
+        Scanner input = new Scanner(System.in);
+        int years = 30;
+        System.out.println("Enter investment amount in $: ");
+        double investmentAmount = input.nextDouble();
+        System.out.println("Enter annual interest rate in percentage: ");
+        double interestRate = input.nextDouble();
+
+        //divides on both sides
+        interestRate /= 1200;
+
+        //runs futureInvestmentValue method, with inputs
+        //sets return to "result"
+        double result = futureInvestmentValue(investmentAmount, interestRate, years);
+
+        //sout result
+        System.out.println("Future valid in 30 years is: " + "$" + result);
+
+    }
+    //Method type double, takes 3 inputs
+    //returns computed result
+    public static double futureInvestmentValue(double inventmentAmount, double monthlyInterestRate, int years) {
+        //computes future investment amount in 30 years
+        //sets to "futureInvestment"
+        double futureInvestment = inventmentAmount * Math.pow(1 + monthlyInterestRate, years * 12);
+
+        return futureInvestment;
+    }
 }
