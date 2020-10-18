8124e3197829c1b2e29f71501d1cf1f24fe5982c
patrickmylius <patrickmylius@outlook.dk>
2020-10-18 16:07:22 +0200

    06_12

--- src/Chap6/Exercise_06_12/Main.java
+++ src/Chap6/Exercise_06_12/Main.java
@@ -7,9 +7,24 @@
  */
 package Chap6.Exercise_06_12;
 
+import java.util.Random;
+
 /** Display numbers */
 //Write a method that prints numbers using the following header:
 //public static void printNumbers(int num1, int num2, int num3, numberPerLine)
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printNumbers(int num1, int num2, int numberPerLine) {
+        final int CHARS_PER_LINE = 10;
+        final int MAX_VALUE = 100;
+
+        for (int i = 0; i < MAX_VALUE ; i++) {
+
+        }
+
+    }
 }
