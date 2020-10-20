78e94062a0f2e3e23ef58bbf6ff191fd69a22c40
Patrick <patr6996@edu.easj.dk>
2020-10-20 12:33:40 +0200

    Exercise_06_30 added. w notes

--- src/Chap6/Exercise_06_30/Main.java
+++ src/Chap6/Exercise_06_30/Main.java
@@ -7,6 +7,8 @@
  */
 package Chap6.Exercise_06_30;
 
+import java.util.Random;
+
 /**
  * Game: craps
  */
@@ -16,4 +18,55 @@ package Chap6.Exercise_06_30;
 //you lose: if the sum is 7 or 11 (called natural), you win; if the sum is another value
 //blablablabla to much text
 public class Main {
+
+    public static void main(String[] args) {
+
+        //For loop running method until losers point are hit
+        for (int i = 0; i <= 100; i++) {
+            System.out.println("Roll the dices!");
+            //Runs rollDice method, sets return to int "points"
+            int point = rollDice();
+            //Runs gameResult method, sets return to String "result"
+            String result = gameResult(point);
+            //souts result
+            System.out.println(result);
+            //If statement, breaks if losersPoint are rolled.
+            if (point == 3 || point == 2 || point == 12) {
+                System.exit(0);
+            }
+        }
+    }
+
+    //Method that generate two random rolls
+    //Max bounds set to 6 pr. dice
+    //returns total point
+    public static int rollDice() {
+        Random randomNumb = new Random();
+        int dice1 = randomNumb.nextInt(6) + 1;
+        int dice2 = randomNumb.nextInt(6) + 1;
+
+        System.out.println("You rolled " + dice1 + " + " + dice2 + " = " + (dice1 + dice2));
+
+        int point = dice1 + dice2;
+
+        return point;
+    }
+
+    //Method that computes if points results in a win or lose
+    //Contains if statements to check points
+    //Returns String win or lose.
+    public static String gameResult(int point) {
+        if (point == 7 || point == 11) {
+            String win = "You win!\n";
+            return win;
+
+        } else if (point == 2 || point == 3 || point == 12) {
+            String lose = "You loose!\n";
+            return lose;
+        } else {
+            String natural = "You win\n";
+            return natural;
+        }
+    }
+
 }
