bef3c773f52c6787ef53c954c04db59e0267c2c3
patrickmylius <patrickmylius@outlook.dk>
2020-10-25 14:00:45 +0100

    Exercise_09_02 Fan Class implemented, holding Mutator and accessor methods, along with Fan construct
    Exercise_09_02 TestFan Class implementing, creating fan1 and fan2 objects.
    
    w. notes

--- src/Chap9/Exercise_09_08/TestFan.java
+++ src/Chap9/Exercise_09_08/TestFan.java
@@ -6,9 +6,53 @@
  * 25-10-2020
  */
 package Chap9.Exercise_09_08;
+/**********************************************************************************
+ * (The Fan class) Design a class named Fan to represent a fan. The class contains:*
+ *                                                                                 *
+ * ■ Three constants named SLOW, MEDIUM, and FAST with the values 1, 2, and 3 to   *
+ *   denote the fan speed.                                                         *
+ * ■ A private int data field named speed that specifies the speed of the fan (the *
+ *   default is SLOW).                                                             *
+ * ■ A private boolean data field named on that specifies whether the fan is on    *
+ *   (the default is false).                                                       *
+ * ■ A private double data field named radius that specifies the radius of the fan *
+ *   (the default is 5).                                                           *
+ * ■ A string data field named color that specifies the color of the fan           *
+ *   (the default is blue).                                                        *
+ * ■ The accessor and mutator methods for all four data fields.                    *
+ * ■ A no-arg constructor that creates a default fan.                              *
+ * ■ A method named toString() that returns a string description for the fan. If   *
+ *   the fan is on, the method returns the fan speed, color, and radius in one     *
+ *   combined string. If the fan is not on, the method returns the fan color and   *
+ *   radius along with the string “fan is off” in one combined string.             *
+ *   the second object. Display the objects by                                     *
+ * invoking their toString method.                                                 *
+ /*********************************************************************************/
 
 public class TestFan {
     public static void main(String[] args) {
+        final int SLOW = 1;
+        final int MEDIUM = 2;
+        final int FAST = 3;
 
+        //Creating two fan objects
+        Fan fan1 = new Fan();
+        Fan fan2 = new Fan();
+
+        //Assign fan1 speed, radius, color
+        fan1.setSpeed(FAST);
+        fan1.setRadius(10);
+        fan1.setFanColor("Yellow");
+        fan1.powerOn();
+
+        //Assign fan2 speed, radius, color
+        fan2.setSpeed(MEDIUM);
+        fan2.setRadius(5);
+        fan2.setFanColor("Blue");
+        fan2.powerOff();
+
+        //Display objects, invoking toStrings() methods
+        System.out.println(fan1.toString());
+        System.out.println(fan2.toString());
     }
 }
