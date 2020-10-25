5a25e82996a2efecbf3df2f8c280e52527507095
patrickmylius <patrickmylius@outlook.dk>
2020-10-25 11:08:28 +0100

    Implemented Listing 9.1 TestCircle.

---
+++ src/Chap9/Circle.java
@@ -0,0 +1,33 @@
+/**
+ * Developed by Patrick Grønvold
+ * patr6996@edu.easj.dk
+ * Student of Computer Science
+ * Zealand ErhvervsAkademi - 4700
+ * 25-10-2020
+ */
+package Chap9;
+
+public class Circle {
+    double radius = 1;
+
+    Circle() {
+    radius = 1;
+    }
+
+    Circle(double newRadius) {
+    radius = newRadius;
+    }
+
+    double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+    void setRadius(double newRadius) {
+        radius = newRadius;
+    }
+}
+
