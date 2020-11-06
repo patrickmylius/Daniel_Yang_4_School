f6da3e27878595776ff8c5f3b413d66ff749346e
patrickmylius <patrickmylius@outlook.dk>
2020-11-06 13:43:43 +0100

    Added 10_01 Time, creating no arg construct. w notes.

--- src/Chap10/Exercise_10_01/Level_01/Time.java
+++ src/Chap10/Exercise_10_01/Level_01/Time.java
@@ -10,27 +10,27 @@ package Chap10.Exercise_10_01.Level_01;
 import java.util.Date;
 
 public class Time {
+    //create object CurrentTime, holds Date
     Date currentTime = new Date();
-
+    //data fields, hour, minute and second
     int hour;
     int minute;
     int second;
 
-
+    //Method, returning hour, minute and second.
     private void returnCurrentTime() {
-        currentTime.getTime();
-        
         this.hour = currentTime.getHours();
         this.minute = currentTime.getMinutes();
         this.second = currentTime.getSeconds();
     }
-
+    //no arg construct, returns current time
     Time() {
         returnCurrentTime();
     }
 
 
     public static void main(String[] args) {
+        //Object time1 with current time.
         Time time1 = new Time();
         System.out.println("Clock is: " + time1.hour + ":" + time1.minute + ":" + time1.second);
 
